d89419cdc5341ae446f304ca05c200f9c05265e4
MannySauce <33158848+MannySauce@users.noreply.github.com>
2023-04-21 12:43:57 -0700

    home screen, and tests lib

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,18 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 
 export default function Home() {
     return (
-        <View style={styles.screenContainer}>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={styles.screenContainer}>
+            <View style={{ marginHorizontal: 20 }}>
+                <Text style={styles.screenTitleText}>Bienvenido de vuelta!</Text>
+                <Text style={styles.screenSubTitleText}>Ruben Rodriguez</Text>
+            </View>
+            <View style={{ margin: 20 }}>
+                <Text style={{ fontSize: 14, color: '#9B9898', fontWeight: '800' }}>TUS PUNTOS</Text>
+            </View>
+            <View style={{ backgroundColor: '#334FFA', height: 143, width: 286, alignSelf: 'center' }}>
+
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     screenContainer: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        marginTop: 60
+    },
+    screenTitleText: {
+        fontSize: 20,
+        fontWeight: "800",
+        color: "#020202"
+    },
+    screenSubTitleText: {
+        fontSize: 14,
+        fontWeight: "400",
+        color: "#020202"
     }
 })
