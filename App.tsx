401b7217bcf26fdd877625e5560ec1b91ebbbcff
MannySauce <33158848+MannySauce@users.noreply.github.com>
2023-04-22 01:54:46 -0700

    deleted unnecessary import in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import AppNav from './src/navigation/AppNav';
-import { View } from 'react-native'
 export default function App() {
   return (
     <AppNav></AppNav>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 40,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
