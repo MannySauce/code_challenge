6562bf0e84eccc705e875a76c42917688ac7d8ba
MannySauce <33158848+MannySauce@users.noreply.github.com>
2023-04-21 18:26:45 -0700

    pointsCard, dummy data for movements list

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,7 +1,48 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
 
 export default function Home() {
+    const data = [
+        {
+            "createdAt": "2022-12-09T06:34:25.607Z",
+            "product": "Handmade Metal Shoes",
+            "points": 16434,
+            "image": "https://loremflickr.com/640/480/transport",
+            "is_redemption": false,
+            "id": "1"
+        },
+        {
+            "createdAt": "2022-12-09T17:02:51.904Z",
+            "product": "Recycled Plastic Tuna",
+            "points": 92984,
+            "image": "https://loremflickr.com/640/480/technics",
+            "is_redemption": false,
+            "id": "2"
+        },
+        {
+            "createdAt": "2022-12-09T10:20:00.909Z",
+            "product": "Fantastic Granite Bacon",
+            "points": 42416,
+            "image": "https://loremflickr.com/640/480/technics",
+            "is_redemption": false,
+            "id": "3"
+        },
+        {
+            "createdAt": "2022-12-09T00:30:23.966Z",
+            "product": "Fantastic Fresh Gloves",
+            "points": 23913,
+            "image": "https://loremflickr.com/640/480/city",
+            "is_redemption": true,
+            "id": "4"
+        },
+        {
+            "createdAt": "2022-12-08T18:54:56.243Z",
+            "product": "Rustic Rubber Bacon",
+            "points": 69814,
+            "image": "https://loremflickr.com/640/480/people",
+            "is_redemption": true,
+            "id": "5"
+        }]
     return (
         <SafeAreaView style={styles.screenContainer}>
             <View style={{ marginHorizontal: 20 }}>
@@ -11,9 +52,14 @@ export default function Home() {
             <View style={{ margin: 20 }}>
                 <Text style={{ fontSize: 14, color: '#9B9898', fontWeight: '800' }}>TUS PUNTOS</Text>
             </View>
-            <View style={{ backgroundColor: '#334FFA', height: 143, width: 286, alignSelf: 'center' }}>
-
+            <View style={styles.pointsCard}>
+                <Text style={{ marginLeft: 19, marginTop: 21, marginBottom: 7, color: '#FFF', fontSize: 16, fontWeight: '800', position: 'absolute', left: 0, top: 0 }}>Diciembre</Text>
+                <Text style={{ color: '#FFF', fontSize: 32, fontWeight: '800' }}>10,00.00 pts</Text>
             </View>
+            <View style={{ margin: 20 }}>
+                <Text style={{ fontSize: 14, color: '#9B9898', fontWeight: '800' }}>TUS MOVIMIENTOS</Text>
+            </View>
+
         </SafeAreaView>
     )
 }
@@ -32,5 +78,16 @@ const styles = StyleSheet.create({
         fontSize: 14,
         fontWeight: "400",
         color: "#020202"
+    },
+    pointsCard: {
+        backgroundColor: '#334FFA', height: 143, maxHeight: 143, width: 286, alignSelf: 'center', flex: 1, justifyContent: 'center', alignItems: 'center', borderRadius: 20,
+        shadowColor: "rgba(0,0,0,0.5)",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.17,
+        shadowRadius: 3.05,
+        elevation: 4
     }
 })
