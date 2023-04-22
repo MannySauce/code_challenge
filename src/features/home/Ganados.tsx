4bec0c1e5cc6a7791634dd92aa52e604bbaa7fc0
MannySauce <33158848+MannySauce@users.noreply.github.com>
2023-04-22 03:15:56 -0700

    added style objects

--- src/features/home/Ganados.tsx
+++ src/features/home/Ganados.tsx
@@ -47,52 +47,47 @@ export default function Ganados() {
 
     return (
         <SafeAreaView style={styles.screenContainer}>
-            <View style={{ marginHorizontal: 20 }}>
+            <View style={styles.screenTitleContainer}>
                 <Text style={styles.screenTitleText}>Bienvenido de vuelta!</Text>
                 <Text style={styles.screenSubTitleText}>Ruben Rodriguez</Text>
             </View>
-            <View style={{ margin: 20 }}>
-                <Text style={{ fontSize: 14, color: '#9B9898', fontWeight: '800' }}>TUS PUNTOS</Text>
-            </View>
+            <Text style={styles.sectionTitleText}>TUS PUNTOS</Text>
             <View style={styles.pointsCard}>
-                <Text style={{ marginLeft: 19, marginTop: 21, marginBottom: 7, color: '#FFF', fontSize: 16, fontWeight: '800', position: 'absolute', left: 0, top: 0 }}>Diciembre</Text>
-                <Text style={{ color: '#FFF', fontSize: 32, fontWeight: '800' }}>10,00.00 pts</Text>
+                <Text style={styles.pointsCardMonthText}>Diciembre</Text>
+                <Text style={styles.pointsText}>10,00.00 pts</Text>
             </View>
-            <View style={{ margin: 20 }}>
-                <Text style={{ fontSize: 14, color: '#9B9898', fontWeight: '800' }}>TUS MOVIMIENTOS</Text>
-            </View>
-            <View style={{ marginHorizontal: 20 }}>
+            <Text style={styles.sectionTitleText}>TUS MOVIMIENTOS</Text>
+            <View style={styles.purchaseListAndBtnContainer}>
                 <FlatList
                     data={data}
-                    // contentContainerStyle={{ marginVertical: 20 }}
+                    style={styles.purchaseListContainer}
                     ItemSeparatorComponent={() => <View style={{ height: 8 }}></View>}
                     ListHeaderComponent={() => <View style={{ height: 20 }}></View>}
                     ListFooterComponent={() => <View style={{ height: 20 }}></View>}
                     renderItem={({ item }) => {
                         return (
-                            <TouchableOpacity key={item.id.toString()} activeOpacity={0.7} style={{ height: 55, marginHorizontal: 10, flexDirection: 'row', alignItems: 'center' }}>
+                            <TouchableOpacity key={item.id.toString()} activeOpacity={0.7} style={styles.itemContainer}>
                                 <Image
                                     source={{ uri: 'https://reactjs.org/logo-og.png' }}
-                                    style={{ width: 55, height: 55, borderRadius: 10, marginRight: 11 }}
+                                    style={styles.itemImgContainer}
                                 />
-                                <View style={{ flexDirection: 'column', marginRight: 50 }}>
-                                    <Text style={{ color: '#000', fontSize: 14, fontWeight: '800', marginBottom: 7 }}>Nombre del producto</Text>
-                                    <Text style={{ color: '#000', fontSize: 12, fontWeight: '400' }}>26 de enero, 2019</Text>
+                                <View style={styles.itemDescriptionContainer}>
+                                    <Text style={styles.itemNameText}>Nombre del producto</Text>
+                                    <Text style={styles.itemDateText}>26 de enero, 2019</Text>
                                 </View>
-                                <View style={{ flexDirection: 'row' }}>
-                                    <Text style={{ color: '#00B833', fontSize: 16, fontWeight: '800' }}>+</Text>
-                                    <Text style={{ color: '#000', fontSize: 16, fontWeight: '800' }}>100</Text>
+                                <View style={styles.itemPointsContainer}>
+                                    <Text style={styles.itemOperationIndicatorText}>+</Text>
+                                    <Text style={styles.itemPointsText}>100</Text>
                                 </View>
-                                <View style={{ position: 'absolute', right: 10 }}>
+                                <View style={styles.itemIconContainer}>
                                     <Fontisto name="angle-right" size={12} color="#070707" />
                                 </View>
                             </TouchableOpacity>
                         )
                     }}
-                    style={{ maxHeight: 350, height: 350, backgroundColor: '#fff', borderRadius: 10, }}
                 />
-                <TouchableOpacity activeOpacity={0.7} style={{ marginTop: 43, maxHeight: 50, height: 50, width: '100%', backgroundColor: '#334FFA', alignSelf: 'center', borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ color: '#fff', fontSize: 16, fontWeight: '800' }}>Todos</Text>
+                <TouchableOpacity activeOpacity={0.7} style={styles.todosBtn}>
+                    <Text style={styles.todosBtnText}>Todos</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
@@ -104,6 +99,9 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#F8F8F8'
     },
+    screenTitleContainer: {
+        marginHorizontal: 20
+    },
     screenTitleText: {
         fontSize: 20,
         fontWeight: "800",
@@ -112,7 +110,13 @@ const styles = StyleSheet.create({
     screenSubTitleText: {
         fontSize: 14,
         fontWeight: "400",
-        color: "#020202"
+        color: "#020202",
+    },
+    sectionTitleText: {
+        fontSize: 14,
+        color: '#9B9898',
+        fontWeight: '800',
+        margin: 20
     },
     pointsCard: {
         backgroundColor: '#334FFA',
@@ -132,5 +136,91 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.17,
         shadowRadius: 3.05,
         elevation: 4
+    },
+    pointsCardMonthText: {
+        marginLeft: 19,
+        marginTop: 21,
+        marginBottom: 7,
+        color: '#FFF',
+        fontSize: 16,
+        fontWeight: '800',
+        position: 'absolute',
+        left: 0,
+        top: 0
+    },
+    pointsText: {
+        color: '#FFF',
+        fontSize: 32,
+        fontWeight: '800'
+    },
+    purchaseListAndBtnContainer: {
+        marginHorizontal: 20
+    },
+    //itemSeperator, listHeader, listFooter
+    purchaseListContainer: {
+        maxHeight: 350,
+        height: 350,
+        backgroundColor: '#fff',
+        borderRadius: 10
+    },
+    itemContainer: {
+        height: 55,
+        marginHorizontal: 10,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    itemImgContainer: {
+        width: 55,
+        height: 55,
+        borderRadius: 10,
+        marginRight: 11
+    },
+    itemDescriptionContainer: {
+        flexDirection: 'column',
+        marginRight: 50
+    },
+    itemNameText: {
+        color: '#000',
+        fontSize: 14,
+        fontWeight: '800',
+        marginBottom: 7
+    },
+    itemDateText: {
+        color: '#000',
+        fontSize: 12,
+        fontWeight: '400'
+    },
+    itemPointsContainer: {
+        flexDirection: 'row'
+    },
+    itemOperationIndicatorText: {
+        color: '#00B833',
+        fontSize: 16,
+        fontWeight: '800'
+    },
+    itemPointsText: {
+        color: '#000',
+        fontSize: 16,
+        fontWeight: '800'
+    },
+    itemIconContainer: {
+        position: 'absolute',
+        right: 10
+    },
+    todosBtn: {
+        marginTop: 43,
+        maxHeight: 50,
+        height: 50,
+        width: '100%',
+        backgroundColor: '#334FFA',
+        alignSelf: 'center',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    todosBtnText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '800'
     }
 })
