cf81d6bced060c4d9d44cc579227bcb72ab79099
MannySauce <33158848+MannySauce@users.noreply.github.com>
2023-04-23 01:16:14 -0700

    added Todos Screen, onpress nav

--- src/navigation/AppNav.tsx
+++ src/navigation/AppNav.tsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GanadosScreen from '../features/ganados/Ganados';
-
+import TodosScreen from '../screens/Todos';
 const Stack = createNativeStackNavigator();
 
 function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Home" component={GanadosScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Todos" component={TodosScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Ganados" component={GanadosScreen} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
